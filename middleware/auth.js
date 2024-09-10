2aa9024a8bd25ebee372f2e1c7fc5994987d59d2
Kazbonfim <zcry.games@gmail.com>
2024-09-10 03:46:29 -0300

    Pequenos ajustes, para fins de debug...

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,11 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
 
+    // https://www.youtube.com/watch?v=94FonmsT27s > 1:10:12
     console.log(req);
+    const token = req.header.authorization;
     // const token = '';
+    next();
 
 };
 
