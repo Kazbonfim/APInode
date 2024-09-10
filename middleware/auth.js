dbbc1442e81d1f085aca4da71376f7dc83fb7497
Kazbonfim <zcry.games@gmail.com>
2024-09-10 10:39:41 -0300

    Implementação de criptografia feita!

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,11 +5,28 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const auth = (req, res, next) => {
 
     // https://www.youtube.com/watch?v=94FonmsT27s > 1:10:12
-    console.log(req);
-    const token = req.header.authorization;
-    // const token = '';
-    next();
+    const token = req.headers.authorization;
+    console.log(token);
+    
 
+    if (!token) {
+        console.log("Ops! 😭");
+        
+        return res.status(401).json({ message: "Acesso negado" })
+    }
+
+    try {
+
+        const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET);
+
+        req.userId = decoded.id;    
+
+    } catch (error) {
+        console.log(error + "😭");
+        return res.status(401).json({ message: "Token inválido!" })
+    }
+
+    next();
 };
 
 module.exports = auth;
