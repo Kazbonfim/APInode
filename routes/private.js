91b14fe8beef312005714a8245d874aa30e19209
Kazbonfim <zcry.games@gmail.com>
2024-09-14 15:54:11 -0300

    Ajustes finos

--- routes/private.js
+++ routes/private.js
@@ -1,12 +1,12 @@
 // Dependências
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
+
 // Controladores
 const { filterDatabase } = require('../controller/filterDatabase'); // [1]
 const { updateUser } = require('../controller/updateUser'); // [2]
+const { deleteUser } = require('../controller/deleteUser');
 
 // Instância do Prisma
 const prisma = new PrismaClient();
@@ -14,6 +14,8 @@ const prisma = new PrismaClient();
 // Rotas
 router.get('/database', filterDatabase); // [1]
 
-router.put('/update/:id', updateUser); // [2]
+router.put('/update/:id', updateUser);
+ // [2]
+router.delete('/delete/:id', deleteUser); // [2]
 
 module.exports = router;
