c149d5c0b08c19514c01c45c86bcbe70cb388b4b
Kazbonfim <zcry.games@gmail.com>
2024-09-14 02:07:28 -0300

    Revert "Nova organização, para praticar modularização, e padrão MVC"
    This reverts commit 7a78defe5223e788ee0c9a6c9764e013fd52a9c1.

--- utils/config.js
+++ utils/config.js
@@ -1,25 +1,20 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const saltRounds = 10; // Valor | Força de criptografia a ser usada
+const saltRounds = 10; // Criptografia de senhas, antes de enviar ao DB
 
 // Funções utilitárias de segurança
-
-// Gerar um novo Token
 const generateToken = (payload, secret, options) => {
     return jwt.sign(payload, secret, options);
 };
 
-// Verificar Token gerado anteriormente
 const verifyToken = (token, secret, options) => {
     return jwt.verify(token, secret, options);
 };
 
-// Criptografar dados e senhas recebidas, antes de armazenar em algo
 const hashPassword = (password) => {
     return bcrypt.hash(password, saltRounds);
 };
 
-// Comparar Token e criptografia
 const comparePassword = (password, hash) => {
     return bcrypt.compare(password, hash);
 };
